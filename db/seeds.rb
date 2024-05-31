af5320700df5057c15e76235135e8f078077601a
AkashSingh26 <akashs3096@gmail.com>
2024-05-31 10:37:37 +0530

    seed file added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+50.times do
+  villa = Villa.create(name: Faker::Address.unique.community)
+  (Date.new(2021, 1, 1)..Date.new(2021, 12, 31)).each do |date|
+    villa.calendar_entries.create(
+      date: date,
+      price: rand(30000..50000),
+      available: [true, false].sample
+    )
+  end
+end
